56f78aa005fbb4bb6af43d2bd8614e363cdc73de
jdavidrice <jdavidrice@gmail.com>
2020-11-24 12:03:18 -0500

    testing layout

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+document.getElementById("jumbotron").innerHTML = `
+  <h1 class="display-3">Work Day Scheduler</h1>
+  <p class="lead">A simple calendar app for scheduling your work day</p>
+  <p id="currentDay" class="lead"></p>
+`;
+
+document.getElementById("timeblock").innerHTML = `
+  <input type="text" class="text-area" placeholder="Enter activity here">
+  </input>
+`;
