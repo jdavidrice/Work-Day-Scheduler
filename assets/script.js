e73d1a00903eec750999d9a0b13f9d80738bc92f
jdavidrice <jdavidrice@gmail.com>
2020-11-25 12:19:02 -0500

    learning about moment

--- assets/script.js
+++ assets/script.js
@@ -8,3 +8,27 @@ document.getElementById("timeblock").innerHTML = `
   <input type="text" class="text-area" placeholder="Enter activity here">
   </input>
 `;
+
+// Current date and time
+let m = moment();
+
+// // Create from ISO 8601 string
+// m = moment("2020-11-25T12:07:00.000-05:00");
+
+// // Using a format BEST WAY
+// m = moment("06/14/2020 3:30PM", "MM/DD/YYYY h:mmP")
+
+// // Create using milliseconds since epoch (Jan 1st 1970)
+// m = moment(600000);
+
+// // Crate using seconds since epoch (Jan 1st 1970)
+// m = moment.unix(7200);
+
+// // Create a moment object in UTC mode
+// m = moment.utc("2020-11-25T12:07:00.000-05:00");
+
+
+
+console.log(`toString() => ${m.toString()}`); // Local mode
+console.log(`toISOString() => ${m.toISOString()}`); // UTC mode
+
